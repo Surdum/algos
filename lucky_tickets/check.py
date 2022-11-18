afdc169943bcd8e93a4a177ed6bfff3b72658f2a
g.babaev <g.babaev@qiwi.com>
2022-11-19 01:59:34 +0600

    add tests

--- lucky_tickets/check.py
+++ lucky_tickets/check.py
@@ -10,3 +10,7 @@ if __name__ == '__main__':
     # my.show_time_graph()
     my = AlgoChecker(FastLuckyNumbersCounter)  # really fast
     my.check(10, '1.Tickets')
+
+    my.run_one_test(10, 3081918923741896840)
+    my.run_one_test(20, 218768894829904122626725603838896148680)
+
