85daf52b93bb6dc75f55c7a8790eb9862ffb199f
g.babaev <g.babaev@qiwi.com>
2023-02-14 21:10:28 +0300

    #

--- sort/shell/shell_sort.py
+++ sort/shell/shell_sort.py
@@ -4,64 +4,13 @@ from utils import generate_random_number_sequence
 
 class ShellSort(AlgoBase, Sort):
     description = "Shell sort"
+    delimiter = 2
 
     def prepare_args(self, *args):
         return args
 
     def run(self, array):
-        cmp = 0
-        asg = 0
-        gap = len(array) // 2
-        while gap > 0:
-            for i in range(gap, len(array)):
-                j = i
-                cmp += 1
-                while j >= gap and array[j - gap] > array[j]:
-                    t = array[j - gap]
-                    array[j - gap] = array[j]
-                    array[j] = t
-                    j -= gap
-                    asg += 1
-            gap //= 2
-        self.vars['cmp'] = cmp
-        self.vars['asg'] = asg
-        return array
-
-
-class ShellSort4(AlgoBase, Sort):
-    description = "Shell sort"
-
-    def prepare_args(self, *args):
-        return args
-
-    def run(self, array):
-        cmp = 0
-        asg = 0
-        gap = len(array) // 4
-        while gap > 0:
-            for i in range(gap, len(array)):
-                j = i
-                cmp += 1
-                while j >= gap and array[j - gap] > array[j]:
-                    t = array[j - gap]
-                    array[j - gap] = array[j]
-                    array[j] = t
-                    j -= gap
-                    asg += 1
-            gap //= 2
-        self.vars['cmp'] = cmp
-        self.vars['asg'] = asg
-        return array
-
-
-class ShellSort8(AlgoBase, Sort):
-    description = "Shell sort"
-
-    def prepare_args(self, *args):
-        return args
-
-    def run(self, array):
-        gap = len(array) // 8
+        gap = len(array) // self.delimiter
         while gap > 0:
             for i in range(gap, len(array)):
                 j = i
@@ -71,6 +20,22 @@ class ShellSort8(AlgoBase, Sort):
                     j -= gap
             gap //= 2
         return array
+
+
+class ShellSort4(ShellSort):
+    description = "Shell sort"
+    delimiter = 4
+
+    def run(self, array):
+        return super().run(array)
+
+
+class ShellSort8(ShellSort):
+    description = "Shell sort"
+    delimiter = 8
+
+    def run(self, array):
+        return super().run(array)
 
 
 if __name__ == '__main__':
