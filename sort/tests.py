e1db69f1df376e513f9e80af8502c6ada7e6b767
g.babaev <g.babaev@qiwi.com>
2023-02-16 21:38:09 +0300

    #

--- sort/tests.py
+++ sort/tests.py
@@ -1,26 +1,21 @@
 from sort import *
 from testers.performance import SortPerformanceComparator
-from utils import generate_shuffled_number_sequence, generate_random_number_sequence
+from utils import generate_shuffled_number_sequence as shf, generate_random_number_sequence as rnd
 import sys
 
 
 sys.setrecursionlimit(1_000_000)
 
-tester = SortPerformanceComparator()
 
-seq_10_2 = generate_shuffled_number_sequence(10**2)
-seq_10_3 = generate_shuffled_number_sequence(10**3)
-seq_10_4 = generate_shuffled_number_sequence(10**4)
-seq_10_5 = generate_shuffled_number_sequence(10**5)
-seq_10_6 = generate_shuffled_number_sequence(10**6)
 
-rnd_10_2 = generate_random_number_sequence(10**2, 999)
-rnd_10_3 = generate_random_number_sequence(10**3, 999)
-rnd_10_4 = generate_random_number_sequence(10**4, 999)
-rnd_10_5 = generate_random_number_sequence(10**5, 999)
-rnd_10_6 = generate_random_number_sequence(10**6, 999)
+seq_10_2 = shf(10**2)
+seq_10_3 = shf(10**3)
+seq_10_4 = shf(10**4)
+seq_10_5 = shf(10**5)
+seq_10_6 = shf(10**6)
 
 # slow
+# tester = SortPerformanceComparator()
 # tester.calculate(algorithms=[BubbleSort, BubbleSortOptimized, InsertionSort, InsertionSortOptimized,
 #                              InsertionSortShift, InsertionSortBinary,
 #                              SelectionSort, HeapSort, ],
@@ -29,14 +24,41 @@ rnd_10_6 = generate_random_number_sequence(10**6, 999)
 #                  iterations=5)
 # tester.print_results()
 
+##########################
+
 # fast
+# tester = SortPerformanceComparator()
 # tester.calculate(algorithms=[ShellSort, ShellSort4, ShellSort8, HeapSortRecursive, QuickSort, MergeSort],
-#                  dataset=[[seq_10_2], [seq_10_3], [seq_10_4], [seq_10_5], ],  # [seq_10_6]],
+#                  dataset=[[seq_10_2], [seq_10_3], ],  # [seq_10_4], [seq_10_5], ],  # [seq_10_6]],
 #                  additional_vars=["cmp", "asg"],
 #                  iterations=20)
 # tester.print_results()
 
+##############################
+
+
+# tester = SortPerformanceComparator()
+# for i in range(5):  # прогоны с разными данными и усредненнием времени
+#     seq_10_2 = shf(10 ** 2)
+#     seq_10_3 = shf(10 ** 3)
+#
+#     tester.calculate(algorithms=[ShellSort, ],
+#                      dataset=[[seq_10_2], [seq_10_3], ],
+#                      additional_vars=["cmp", "asg"],
+#                      iterations=20)
+# tester.print_results()
+
+
+#################################
+
+rnd_10_2 = rnd(10**2, 999)
+rnd_10_3 = rnd(10**3, 999)
+rnd_10_4 = rnd(10**4, 999)
+rnd_10_5 = rnd(10**5, 999)
+rnd_10_6 = rnd(10**6, 999)
+
 # very fast
+tester = SortPerformanceComparator()
 tester.calculate(algorithms=[BucketSort, CountingSort, RadixSort],
                  dataset=[[rnd_10_2], [rnd_10_3], [rnd_10_4], [rnd_10_5], [rnd_10_6]],
                  additional_vars=["cmp", "asg"],
